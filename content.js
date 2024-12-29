8564fb4cb4c5877f2981dc19c4fbd82e7103128e
AliMoamen <alimoamen100@gmail.com>
2024-12-28 20:28:59 -0800

    Add reset to original submission button

--- content.js
+++ content.js
@@ -1,19 +1,34 @@
 console.log("Writing Extension Loaded!");
 
 // Get the content of the writing submission
-const writingSubmission = document.querySelector(
+const writingSubmission = document.querySelectorAll(
   'p[style="user-select: text;"]'
-).innerHTML;
+)[1].innerText;
 
-// Create a new input element dynamically
+// Create a new textarea element dynamically
 const textareaElement = document.createElement("textarea");
 textareaElement.innerHTML = writingSubmission;
-textareaElement.style = 'height: "500px";';
+
+// Set the dimensions of the textarea
+textareaElement.style.height = "300px";
+textareaElement.style.width = "100%";
+
+// Create a reset button
+const resetButton = document.createElement("button");
+resetButton.textContent = "Reset to Original Submission";
+resetButton.style.marginBottom = "10px"; // Add some spacing
+
+resetButton.style.display = "block"; // Make it appear on a new line
+
+// Add a click event listener to reset the textarea content
+resetButton.addEventListener("click", () => {
+  textareaElement.value = writingSubmission;
+});
 
 // Find the reference element
 const referenceElement = document.querySelector(
   'div[class="py4 undo-collapse"]'
 );
 
-// Insert the input element before the reference element
+referenceElement.parentNode.insertBefore(resetButton, referenceElement);
 referenceElement.parentNode.insertBefore(textareaElement, referenceElement);
