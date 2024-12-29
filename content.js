12941b779b7f0b1999b3731b886c492aab1e38b0
AliMoamen <alimoamen100@gmail.com>
2024-12-28 20:06:30 -0800

    Copy Submission and add it to text area

--- content.js
+++ content.js
@@ -1,1 +1,19 @@
 console.log("Writing Extension Loaded!");
+
+// Get the content of the writing submission
+const writingSubmission = document.querySelector(
+  'p[style="user-select: text;"]'
+).innerHTML;
+
+// Create a new input element dynamically
+const textareaElement = document.createElement("textarea");
+textareaElement.innerHTML = writingSubmission;
+textareaElement.style = 'height: "500px";';
+
+// Find the reference element
+const referenceElement = document.querySelector(
+  'div[class="py4 undo-collapse"]'
+);
+
+// Insert the input element before the reference element
+referenceElement.parentNode.insertBefore(textareaElement, referenceElement);
